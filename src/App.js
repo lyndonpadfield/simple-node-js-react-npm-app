ff3725cff438d22b591826770564ff609bfdc6da
LJPBOT <lyndon.padfield@gmail.com>
2018-05-21 22:00:52 +1200

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          My Name is Lyndon Padfield and this is the latest update
         </p>
       </div>
     );
